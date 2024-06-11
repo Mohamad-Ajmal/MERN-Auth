bf726750ce48a79b5c4fb9c2ce5718d84698026e
Mohammad Ajmal Ibrahimkhail <Ajmalibrahimkhail121@gmail.com>
2024-06-11 15:43:50 +0430

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (re, res) => {
+  res.json({
+    message: "API is working!",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -18,3 +20,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
