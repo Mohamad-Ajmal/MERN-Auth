2069ba37d654e406ee5fa02b83397676f7cc1633
Mohammad Ajmal Ibrahimkhail <Ajmalibrahimkhail121@gmail.com>
2024-07-04 10:40:22 +0430

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -16,8 +17,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use(express.json());
 
 app.use(cookieParser());
